132bc7791a1db9dd38112355f51d89d803295461
maximgagarin <max.emercom@yandex.ru>
2024-05-02 22:36:10 +0300

    first commit

---
+++ resources/views/components/left-menu.blade.php
@@ -0,0 +1,17 @@
+<ul class="nav flex-column">
+    <li class="nav-item bg-success">
+        <a class="nav-link text-white"  href="#">График подачи воды</a>
+    </li>
+    <li class="nav-item bg-success">
+        <a class="nav-link text-white" href="{{route('works')}}">Выполненные работы</a>
+    </li>
+    <li class="nav-item bg-success">
+        <a class="nav-link text-white" href="#">Расписание автобусов</a>
+    </li>
+    <li class="nav-item bg-success">
+        <a class="nav-link text-white" href="#">Правление</a>
+    </li>
+    <li class="nav-item bg-success">
+        <a class="nav-link text-white" href="#">Реквизиты</a>
+    </li>
+</ul>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-between head">
+        <div class="col-4">
+       git
+            <p  style=" margin-top: 0 ">садоводческое некоммерческое товарищество</p>
+        </div>
+        <div class="col-4">
+            <h5 class="text-center m-3">тел. 8-999-999-99-99</h5>
+            <p class=" text-center">e-mail: snt@snt.ru</p>
+        </div>
+    </div>
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNavDarkDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle" href="{{route('main')}}" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                Главная
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle text-white" href="{{route('news')}}" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                Новости
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                О товариществе
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                Решения собраний
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">1</a></li>
+                                <li><a class="dropdown-item" href="#">2n</a></li>
+                                <li><a class="dropdown-item" href="#">3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                Документы
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown" id="navbarDropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                                Контакты
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="" aria-label="Найти">
+                        <button class="btn btn-dark" type="submit">Поиск</button>
+
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="row mt-5 ">
+        <div class="col-2 ">
+            <x-left-menu />
+        </div>
+        <div class="col-6 "  style="margin-left: 40px">
+         @yield('content')
+        </div>
+    </div>
+</div>
+</body>
+</html>
