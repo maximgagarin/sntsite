2eedea3feec75508d2ba5a5147f75b1ef9c0d643
maximgagarin <max.emercom@yandex.ru>
2024-05-04 13:19:52 +0300

    04.05.1300

---
+++ resources/views/docs.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('content')
+    <div id="works">
+        <h5> 02.11.2023 год. Засыпка ям на 11 улице возле участков №930.931.932</h5>
+        <img src="{{ asset('images/work1.jpg') }}" alt="Amazing Image">
+    </div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,14 +12,21 @@
 </head>
 <body>
 <div class="container">
-    <div class="row justify-content-between head">
-        <div class="col-4">
-            <h2 class="mt-3"  style=" margin: 0 ">СНТ "ЗАРЯ-2"</h2>
-            <p  style=" margin-top: 0 ">садоводческое некоммерческое товарищество</p>
+    <div class="row justify-content-between head bg-success ">
+        <div class="col-5 mt-2">
+            <div class="row">
+          <div class="col-2">
+              <img src="{{ asset('images/logo5.jpg') }}" alt="">
+          </div>
+            <div class="col-6">
+                <h2    style=" margin: 0; color: white "><span class="text-warning">СНТ</span> "ЗАРЯ-2"</h2>
+                <p  style=" margin-top: 0; font-size: 12px; color: white ">садоводческое некоммерческое товарищество</p>
+            </div>
+            </div>
         </div>
-        <div class="col-4">
-            <h5 class="text-center m-3">тел. 8-999-999-99-99</h5>
-            <p class=" text-center">e-mail: snt@snt.ru</p>
+        <div class="col-2">
+            <span class=" mt-3 text-warning " style="font-size: 16px; display: block">тел. 8-999-999-99-99</span>
+            <span class="text-center  text-white">e-mail: snt@snt.ru</span>
         </div>
     </div>
     <div class="row">
@@ -28,10 +35,10 @@
                 <div class="collapse navbar-collapse justify-content-between" id="navbarNavDarkDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown" id="navbarDropdown">
-                            <a class="nav-link dropdown-toggle" href="{{route('main')}}" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="{{route('main')}}" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
                                 Главная
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                            <ul class="dropdown-menu dropdown-menu-dark text-white" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
                                 <li><a class="dropdown-item" href="#">Another action</a></li>
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
@@ -52,7 +59,7 @@
                     </ul>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown" id="navbarDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
                                 О товариществе
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -64,7 +71,7 @@
                     </ul>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown" id="navbarDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
                                 Решения собраний
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -76,7 +83,7 @@
                     </ul>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown" id="navbarDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
                                 Документы
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -88,7 +95,7 @@
                     </ul>
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown" id="navbarDropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink" role="button" aria-expanded="false">
                                 Контакты
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
@@ -100,8 +107,7 @@
                     </ul>
                     <form class="d-flex">
                         <input class="form-control me-2" type="search" placeholder="" aria-label="Найти">
-                        <button class="btn btn-dark" type="submit">Поиск</button>
-
+                        <button class="btn btn-primary" type="submit">Поиск</button>
                     </form>
                 </div>
             </div>
@@ -113,6 +119,11 @@
         </div>
         <div class="col-6 "  style="margin-left: 40px">
          @yield('content')
+        </div>
+    </div>
+    <div class="row">
+        <div class="footer">
+            <img id="footer_img" src="{{ asset('images/Amazing5.jpg') }}" alt="">
         </div>
     </div>
 </div>
